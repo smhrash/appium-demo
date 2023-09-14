63138dfbae419026d3815a47c1a2809db4ae433f
Sarker M Rashid <sarkerm.rashid@perficient.com>
2023-09-14 10:24:09 -0400

    test case added

--- src/test/java/com/perficient/AppiumBasic.java
+++ src/test/java/com/perficient/AppiumBasic.java
@@ -2,8 +2,11 @@ package com.perficient;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -11,11 +14,26 @@ public class AppiumBasic {
 
     @Test
     public void appiumTest() throws MalformedURLException {
+        String appiumJSPath = "C:\\Users\\sarkerm.rashid\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js";
+
+        // Define the AppiumServiceBuilder with the desired configuration
+        AppiumServiceBuilder builder = new AppiumServiceBuilder()
+                .withAppiumJS(new File(appiumJSPath))
+                .withIPAddress("127.0.0.1")
+                .usingPort(4723);
+
+        // Start the Appium server
+        AppiumDriverLocalService service = builder.build();
+        service.start();
 
         UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Pixel XL API 34");
-        options.setApp("");
+        options.setDeviceName("JovanEmulator");
+        options.setApp("C:\\Users\\sarkerm.rashid\\IdeaProjects\\appium-demo\\src\\test\\java\\com\\perficient\\rosources\\ApiDemos-debug.apk");
 
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), null);
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+
+        driver.quit();
+        service.stop();
     }
+
 }
